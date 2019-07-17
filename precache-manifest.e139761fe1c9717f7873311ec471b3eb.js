3497b673923bd527e872893b138e96c3b2ec1b30
kozi <pkozubek92@gmail.com>
2019-07-17 21:41:52 +0200

    Updates

---
+++ precache-manifest.e139761fe1c9717f7873311ec471b3eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74d260c0c9e17872804b5a8acfe99a97",
+    "url": "/bear-guru/index.html"
+  },
+  {
+    "revision": "6c4a046dcbbdd0a5a699",
+    "url": "/bear-guru/static/css/main.a7fcd8db.chunk.css"
+  },
+  {
+    "revision": "58ccdbd6c37eff1540c1",
+    "url": "/bear-guru/static/js/2.756505ee.chunk.js"
+  },
+  {
+    "revision": "6c4a046dcbbdd0a5a699",
+    "url": "/bear-guru/static/js/main.8683de90.chunk.js"
+  },
+  {
+    "revision": "6f2850708f5b251aaac9",
+    "url": "/bear-guru/static/js/runtime~main.28f33897.js"
+  }
+]);
